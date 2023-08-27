ab8f0e6ff6ca55059a4af841cdaa9bfb5d7650bd
Mariana Gonzalez <142275783+marglezc@users.noreply.github.com>
2023-08-27 14:16:21 -0600

    Add files via upload

--- prueba lista_alumnos.c
+++ prueba lista_alumnos.c
@@ -4,7 +4,7 @@
 
 struct alumno{
 
-    char *nombre;
+    char *nombre; //cadena es puntero
     int creditos;
     int semestre;
 
@@ -15,8 +15,8 @@ typedef Alumno *AlumnoPtr;
 
 struct nodo
 {
-    Alumno alum;
-    struct nodo *enlace;
+    AlumnoPtr alum; //estructura que contiene estructura alumno
+    struct nodo *enlace; //apuntar a siguiente nodo
 };
 
 typedef struct nodo Nodo;
@@ -24,26 +24,23 @@ typedef Nodo *NodoPtr;
 
 NodoPtr crearNodo(AlumnoPtr);
 AlumnoPtr crearAlumno(char*, int, int);
-void insertarNodoOrdenadoCreditos(NodoPtr*, NodoPtr);
+void insertarNodoOrdenadoCred(NodoPtr*, NodoPtr);
 void imprimirLista(NodoPtr);
 
 int main(){
 
     NodoPtr cabecera = NULL;
 
-    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(crearAlumno("Mariana", 94, 3)));
+    insertarNodoOrdenadoCred(&cabecera, crearNodo(crearAlumno("Mariana González Canul", 94, 3)));
     imprimirLista(cabecera);
-    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(crearAlumno("Jaqui", 80, 3)));
+    insertarNodoOrdenadoCred(&cabecera, crearNodo(crearAlumno("Jaqueline Góngora Tun", 88, 3)));
     imprimirLista(cabecera);
-    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(crearAlumno("José Luis", 92, 3)));
+    insertarNodoOrdenadoCred(&cabecera, crearNodo(crearAlumno("José Luis Lara Rubio", 92, 3)));
     imprimirLista(cabecera);
-    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(crearAlumno("Mauro", 90, 3)));
+    insertarNodoOrdenadoCred(&cabecera, crearNodo(crearAlumno("Mauro Arif Kuh Esquivel", 90, 3)));
     imprimirLista(cabecera);
-    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(crearAlumno("Isaías", 86, 3)));
+    insertarNodoOrdenadoCred(&cabecera, crearNodo(crearAlumno("Isaías Rodriguez Couoh", 86, 3)));
     imprimirLista(cabecera);
-
-
-
 
     return 0;
 }
@@ -63,10 +60,56 @@ NodoPtr crearNodo(AlumnoPtr nuevoAlumno){
 
     NodoPtr nuevoNodo = (NodoPtr)malloc(sizeof(Nodo));
 
-    nuevoNodo -> alum.nombre = strdup(nuevoAlumno -> nombre);
-    nuevoNodo -> alum.creditos = nuevoAlumno -> creditos;
-    nuevoNodo -> alum.semestre = nuevoAlumno-> semestre;
+    nuevoNodo -> alum = nuevoAlumno;
     nuevoNodo -> enlace = NULL;
 
     return nuevoNodo;
 }
+
+void insertarNodoOrdenadoCred(NodoPtr* cabecera, NodoPtr nuevoNodo){
+
+    if(*cabecera == NULL){
+        *cabecera = nuevoNodo;
+    }
+    else{
+        NodoPtr actualNodo = *cabecera, anteriorNodo = NULL;
+        while(actualNodo != NULL && nuevoNodo -> alum-> creditos < actualNodo -> alum -> creditos){
+            anteriorNodo = actualNodo;
+            actualNodo = actualNodo -> enlace;
+        }
+
+        if(anteriorNodo == NULL){
+            nuevoNodo -> enlace == actualNodo;
+            *cabecera = nuevoNodo;
+        }
+        else{
+            nuevoNodo -> enlace = anteriorNodo -> enlace;
+            anteriorNodo -> enlace = nuevoNodo;
+        }
+        
+        }
+}
+
+void imprimirLista(NodoPtr cabecera){
+
+    int i = 1;
+    if(cabecera == NULL){
+        printf("\nLista vacia\n");
+        return;
+
+    }
+    else{
+
+        printf("\nLista de alumnos: \n");
+
+        while(cabecera != NULL){
+
+            printf("%d) %s - %d - %d\n", i, cabecera-> alum -> nombre, cabecera-> alum -> creditos, cabecera-> alum -> semestre );
+            cabecera = cabecera -> enlace;
+            i++;
+        }
+        printf("\n");
+    }
+}
+
+
